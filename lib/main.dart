860d0f18ad9e19d993c385eb53313ea463e748d4
Jim Ar Rafi <70357675+JimArRafi10@users.noreply.github.com>
2023-04-10 23:09:05 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+     Quote(author: 'Oscar Wilde', text: 'Be yourself'),
+     Quote(author: 'Oscar Wilde', text: 'Do not quit'),
+     Quote(author: 'Oscar Wilde', text: 'Never Give Up')
+  ];
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+            quote: quote,
+            delete: () {
+              setState(() {
+                quotes.remove(quote);
+              });
+            }
+        )).toList(),
+      )
+    );
+  }
+}
+
+
+
+
+
